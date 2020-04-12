2ddc1b05125897d7405697d86ff28cd5f79c53e3
onair-lena <pechenko@bk.ru>
2020-04-12 17:35:51 +0300

    refactoring

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -2,37 +2,41 @@ import React from 'react'
 
 const MovieTabs = (props) => {
   const { sort_by, updateSortBy } = props
+  
+  const handleClick = (value) => {
+    return () => {
+    updateSortBy(value)
+    }
+  }
+
+const getClassLink = (value) => {
+  return `nav-link ${
+    sort_by === value ? "active" : ""
+    }`
+}
+
   return (
     <div>
       <ul className="tabs nav nav-pills">
         <li className="nav-item">
           <div 
-            className={`nav-link ${
-            sort_by === "popularity.desc" 
-            ? "active" 
-            : ""
-            }`}
-            onClick={() => {
-              updateSortBy("popularity.desc")
-            }}
+            className={getClassLink("popularity.desc")}
+            onClick={handleClick("popularity.desc")}
             >
             Porular
           </div>
         </li>
         <li className="nav-item">
-          <div className={`nav-link ${sort_by === "revenue.desc" ? "active" : ""}`}
-            onClick={() => {
-              updateSortBy("revenue.desc")
-            }}
+          <div className={getClassLink("revenue.desc")}
+            onClick={handleClick("revenue.desc")}
           >
             Revenue
           </div>
         </li>
         <li className="nav-item">
-        <div className={`nav-link ${sort_by === "vote_average.desc" ? "active" : ""}`}
-          onClick={() => {
-            updateSortBy("vote_average.desc")
-          }}
+        {/* <div className={`nav-link ${sort_by === "vote_average.desc" ? "active" : ""}`} */}
+        <div className={getClassLink("vote_average.desc")}
+          onClick={handleClick("vote_average.desc")}
         >
             Vote
           </div>
