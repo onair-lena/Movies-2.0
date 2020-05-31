662da0e56db9ba457f7f9c5a203a6a51b1b46ca7
onair-lena <pechenko@bk.ru>
2020-05-31 15:22:01 +0300

    Updates

---
+++ precache-manifest.2b14fd98c5686d253bbbdc69bc995466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa0dd176388f832b980889be6c7b72f",
+    "url": "/Movies-2.0/index.html"
+  },
+  {
+    "revision": "5f1cf0e0647d27750faa",
+    "url": "/Movies-2.0/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "05b455acec036095426b",
+    "url": "/Movies-2.0/static/css/main.d1978104.chunk.css"
+  },
+  {
+    "revision": "5f1cf0e0647d27750faa",
+    "url": "/Movies-2.0/static/js/2.44c7a01c.chunk.js"
+  },
+  {
+    "revision": "05b455acec036095426b",
+    "url": "/Movies-2.0/static/js/main.bf3f5dfd.chunk.js"
+  },
+  {
+    "revision": "31876471627c07b74116",
+    "url": "/Movies-2.0/static/js/runtime~main.d46c7ba3.js"
+  }
+]);
